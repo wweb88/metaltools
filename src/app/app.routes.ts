5f5b19bc983e4aa8c8134278199a9a98f5d70444
Nelson Espinoza <n.espinoza@tecnoapps.cl>
2026-05-13 17:29:33 -0400

    reglamentos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { SquadToolComponent } from './Pages/squad-tool/squad-tool.component';
 import { LevelCalculatorComponent } from './Pages/level-calculator/level-calculator.component';
+import { ReglamentosComponent } from './Pages/reglamentos/reglamentos.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'reglamentos', component: ReglamentosComponent },
   { path: 'squad-tool', component: SquadToolComponent },
   { path: 'level-calculator', component: LevelCalculatorComponent },
   { path: '**', redirectTo: '' }
